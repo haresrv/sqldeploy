130463b8e1945bcd35727f97bef066daa8174af1
HareSRV <rvharesh00@gmail.com>
2019-07-31 21:33:19 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+constructor(props)
+{
+
+  super(props);
+  this.state={
+    name:'',
+    rollno:''
+  }
 }
 
+handleChange= (event) =>{
+  event.preventDefault();
+  const {name, value} = event.target;
+this.setState({[name]:value});
+console.log(this.state);
+}
+
+  render(){
+  return (
+    <div className="App">
+        <form method='post' action='http://localhost:3001'>
+            <div className='name'>
+              <label htmlFor='name'>Enter Name:</label>
+              <input type='text' name='name' onChange={this.handleChange}/>
+            </div>
+            <div className='rollno'>
+              <label htmlFor='rollno'>Enter Roll-no:</label>
+              <input type='text' name='rollno' onChange={this.handleChange}/>
+            </div>
+            <div className='submit'>
+              <input type='submit'/>
+            </div>            
+        </form>               
+
+     </div>
+  );
+}
+}
 export default App;
